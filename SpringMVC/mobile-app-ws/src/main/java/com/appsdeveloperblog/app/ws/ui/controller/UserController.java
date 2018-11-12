ac0b1ab6696b8d0bce7056f6706ee15bfd1ad4e0
Garbo Cheng <garbocheng93@gmail.com>
2018-11-11 19:50:49 -0800

    Spring Get Controller

--- SpringMVC/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ SpringMVC/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -2,36 +2,46 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("/users")
 public class UserController {
 	
 	@GetMapping
-	public String getUser()
+	public String getUsers(@RequestParam(value="page") int page,
+			@RequestParam(value="limit") int limit)
 	{
-		return "get user was called";
+		return "get users was called page = " + page + " and limit =" + limit;
+	}
+
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId)
+	{
+		return "get user request with userId =" + userId;
 	}
 	
-	@PostMapping
+	@PostMapping 
 	public String createUser()
 	{
-		return "create user was called";
+		return "post user request";
 	}
 	
 	@PutMapping
 	public String updateUser()
 	{
-		return "update user was called";
+		return "update user request";
 	}
 	
 	@DeleteMapping
 	public String deleteUser()
 	{
-		return "delete user was called";
+		return "delete user request";
 	}
+	
 }
